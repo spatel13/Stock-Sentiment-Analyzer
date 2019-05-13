fe82d65abed452bed1954cd129e085ba6eb58ffb
spatel13 <sahil.patel2461@gmail.com>
2019-05-13 15:39:43 -0400

    Integrated the sentimentAnalyzer.py into the tweet scraping

--- tweetScraper.py
+++ tweetScraper.py
@@ -1,6 +1,7 @@
 # Sahil Patel
-# CMSC 473 - NLP
-# spatel32@umbc.edu
+# spatel13
+# sahil.patel2461@gmail.com
+# Last Edited: 5/13/2019
 
 import os
 from pymongo import MongoClient
@@ -44,8 +45,11 @@ class StockStreamer(TwythonStreamer):
         # only want to collect English-language tweets
         if data.get('lang') == 'en':
 
+            # Analyze the sentimentValue and word for the current tweet
             sentimentValue, sentiment = analyzeSentiment(data.get('text'))
-            
+
+            # Minify the overly large tweet with all the metadata to
+            # only the relavent fields
             minified_tweet['text'] = data.get('text')
             minified_tweet['favorite_count'] = data.get('favorie_count')
             minified_tweet['retweet_count'] = data.get('retweet_count')
@@ -54,11 +58,18 @@ class StockStreamer(TwythonStreamer):
             minified_tweet['sentimentValue'] = sentimentValue
             minified_tweet['sentiment'] = sentiment
 
+            # Add the tweet to the Mongo Database
             tweets_db.insert_one(minified_tweet)
 
+            # Print information to the screen (for debugging)
+            # TODO: Switch to logger
             print(self.company.upper() + ":  " + data.get("text"))
+
+            # Increment counter for number of collected tweets
             self.tweetCounter += 1
-            
+
+            # If we have collected 10 or more tweets
+            # we disconnect and move to the next company
             if self.tweetCounter >= 10:
                 self.disconnect()
                         
@@ -69,10 +80,15 @@ class StockStreamer(TwythonStreamer):
         self.disconnect()
 
 if __name__ == '__main__':
+    # List of companies I am keeping track of and the query string for them
     queries = ["tesla,#tesla,@tesla,$TSLA", "facebook,#facebook,@facebook,$FB", "apple,#apple,@apple,$aapl", "google,#google,@google,$goog"]
 
+    # Create the stream I will be using the collect my tweets
+    # based on the custom extended class
     stream = StockStreamer(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
+    # For each query in my list, collect tweets til diconnected,
+    # then sleep for 30 seconds and move to next query.
     while True:
         for query in queries:
             stream.statuses.filter(track=query)
